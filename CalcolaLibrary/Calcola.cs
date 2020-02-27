88143d33cf53058d53ef89b8eae2a0894d9ab39c
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-02-27 16:10:28 +0100

    Aggiungere i file di progetto.

---
+++ CalcolaLibrary/Calcola.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CalcolaLibrary
+{
+    public static class Calcola
+    {
+        public static double Raddoppia(double num)
+        {
+            double resp = num * 2;
+            return resp;
+        }
+        public static double SommaMonete(double[] monete)
+        {
+            double resp = 0;
+            for(int i = 0; i < monete.Length; i++)
+            {
+                resp = resp + monete[i];
+            }
+            return resp;
+        }
+    }
+}
---
+++ CalcolaLibraryTests/CalcolaTests.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalcolaLibraryTests;
+namespace CalcolaLibraryTests
+{
+    [TestClass]
+    public class CalcolaTests
+    {
+        [DataTestMethod]
+        [DataRow(35,70)]
+        [DataRow(50,100)]
+        [DataRow(-6,-12)]
+        public void RaddoppiaTest(double num ,double risposta_aspettata)
+        {
+            double risposta_effettiva = CalcolaLibrary.Calcola.Raddoppia(num);
+            Assert.AreEqual(risposta_aspettata, risposta_effettiva);
+        }
+
+        [TestMethod]
+        public void TestSommaMonete()
+        {
+            double[] monete = new double[5] { 20, 30, 5, 2, 4 };
+            double somma_aspettata = 61;
+            double somma_ricevuta = CalcolaLibrary.Calcola.SommaMonete(monete);
+            Assert.AreEqual(somma_aspettata, somma_ricevuta);
+        }
+    }
+}
